61d0e2c8cedb647ff1a4c9d744f49432ffb20834
amitbhai9161 <amitk095173@gmail.com>
2024-09-19 15:30:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector("#show").addEventListener("click",()=>{
+
+    document.querySelector(".box").style.display ="block";
+})
+
+
+document.querySelector("#hide").addEventListener("click",()=>{
+    document.querySelector(".box").style.display ="none";
+})
